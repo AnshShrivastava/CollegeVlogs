10e9425fb71d67528fcae52632beef600adfb950
Ansh Shrivastava (B17CS005) <shrivastava.4@iitj.ac.in>
2020-10-27 19:21:32 +0530

    Updated Database

--- Vlogedge/Vlogedge/settings.py
+++ Vlogedge/Vlogedge/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'Vlogedge.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -87,7 +87,7 @@ DATABASES = {
         'HOST':'localhost'
     }
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
