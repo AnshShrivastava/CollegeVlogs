f0c704ca88762f2be58b789eda57a9357b284cc6
Ansh Shrivastava (B17CS005) <shrivastava.4@iitj.ac.in>
2020-10-31 12:43:55 +0530

    Updated Postview

--- frontend/views.py
+++ frontend/views.py
@@ -60,7 +60,7 @@ def about_us(request):
 def vlogs(request):
     vlog_id = request.GET.get('vlog_id')
     v = Vlogs.objects.get(id=vlog_id)
-    url = getattr(v, video)
+    url = getattr(v, video.url)
     url.replace("http:", "https:")
     return render(request,'postview.html',{'data':v, 'videourl': url})
 
