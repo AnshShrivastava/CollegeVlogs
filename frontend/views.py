77d248d89d75d52e92f6c2b47608e9e1f33d26cc
Ansh Shrivastava (B17CS005) <shrivastava.4@iitj.ac.in>
2020-11-06 12:33:00 +0530

    Search

--- frontend/views.py
+++ frontend/views.py
@@ -86,7 +86,7 @@ class search(views.APIView):
 
     def get(self,request):
         srch = request.GET['search']
-        data1 = College.objects.filter(collegename__contains=srch).distinct()
+        data1 = Vlogs.objects.filter(college__collegename__contains=srch).distinct()
         # vlog = Vlogs.objects.filter(college_id__in = Vlogs.objects.values('college_id'))
         data2 = Vlogs.objects.filter(title__contains=srch).distinct()
         data = (data1 | data2).distinct()
