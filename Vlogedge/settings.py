06cd2db6369998536bfc6b34b8ae801fb0d3e39b
Ansh Shrivastava (B17CS005) <shrivastava.4@iitj.ac.in>
2021-01-14 13:47:24 +0530

    Change Database for Heroku

--- Vlogedge/settings.py
+++ Vlogedge/settings.py
@@ -80,26 +80,26 @@ WSGI_APPLICATION = 'Vlogedge.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {    
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }    
-# }
-
-DATABASES = {
+DATABASES = {    
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'vlogagge',
-        'USER': 'myadmin@vloggage-app',
-        'PASSWORD': 'ThisIs4P4ssw0rd!=1',
-        'HOST': 'vloggage-app.postgres.database.azure.com',
-        'PORT': 5432,
-        'OPTIONS': {
-            'sslmode': 'require',
-        }
-    }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }    
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'vlogagge',
+#         'USER': 'myadmin@vloggage-app',
+#         'PASSWORD': 'ThisIs4P4ssw0rd!=1',
+#         'HOST': 'vloggage-app.postgres.database.azure.com',
+#         'PORT': 5432,
+#         'OPTIONS': {
+#             'sslmode': 'require',
+#         }
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
